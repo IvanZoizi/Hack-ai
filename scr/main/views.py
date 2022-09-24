a911c726ed76d50c71a794fe50fd46a264fab20d
Zoizi <zooiiz!@yandex.ru>
2022-09-24 17:32:24 +0400

    Скачивание архива

--- scr/main/views.py
+++ scr/main/views.py
@@ -1,8 +1,11 @@
+from django.http import HttpResponseRedirect
+
 from .models import Photo
 from .forms import PhotoForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.files.base import ContentFile
 from .model import get_model
+import zipfile
 
 import cv2
 
@@ -10,17 +13,13 @@ import cv2
 def add_location(request):
     if request.method == 'GET':
         form = PhotoForm()
-        #model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
-        #model.classes = [0]
-        #model.conf = 0.3
-        #print(model([img2]).xyxy)
-        #img = cv2.imread('images/4.JPG_0_0.jpg')
-        #img2 = img[0:640, 0:640]
-        #pred = model(img)
         return render(request, 'image.html', {'form': form})
     elif request.method == 'POST':
         form = PhotoForm(request.POST, request.FILES)
         if form.is_valid():
+            image_list = []
+            file = zipfile.ZipFile('media/photos/' + str(request.FILES.getlist('photos')[0]) + '.zip', 'w')
+            print(1)
             for f in request.FILES.getlist('photos'):
                 data = f.read()
                 photo = Photo()
@@ -38,7 +37,14 @@ def add_location(request):
                         if y + SIDE > h:
                             y_dif = y + SIDE - h
                         pred = get_model(img[y - y_dif:y + SIDE, x - x_dif:x + SIDE])
-                        print(pred)
+                image_list.append(f"{photo.image.url}")
+                file.write(rf"{photo.image.url[1:]}")
+            file.close()
+            return render(request, 'save.html', {'images': image_list[1:], 'img1': image_list[0],
+                                                 'name': str(request.FILES.getlist('photos')[0]) + '.zip'})
+        elif request.POST.get("save_next"):
+            form = PhotoForm()
             return render(request, 'image.html', {'form': form})
         else:
+            form = PhotoForm()
             return render(request, 'image.html', {'form': form})
