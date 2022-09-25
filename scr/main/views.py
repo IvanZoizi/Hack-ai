1a88eb27f82034e22e42a466e9676227f5007dc1
Ivan Zakharov <95079064+IvanZoizi@users.noreply.github.com>
2022-09-25 11:19:10 +0400

    Готово

--- scr/main/views.py
+++ scr/main/views.py
@@ -4,7 +4,7 @@ from .models import Photo
 from .forms import PhotoForm
 from django.shortcuts import render, redirect
 from django.core.files.base import ContentFile
-from .model import get_model, gpu_cpu
+from .model import eval, gpu_cpu
 import zipfile
 
 import cv2
@@ -29,17 +29,17 @@ def add_location(request):
                 h, w, _ = img.shape
                 gpu = form.cleaned_data.get('gpu_or_cpu', False)
                 gpu_cpu(gpu)
-                for x in range(0, w, SIDE):
-                    for y in range(0, h, SIDE):
-                        x_dif = 0
-                        y_dif = 0
-                        if x + SIDE > w:
-                            x_dif = x + SIDE - w
-                        if y + SIDE > h:
-                            y_dif = y + SIDE - h
-                        pred = get_model(img[y - y_dif:y + SIDE, x - x_dif:x + SIDE])
-                image_list.append(f"{photo.image.url}")
-                file.write(rf"{photo.image.url[1:]}")
+    #"""                for x in range(0, w, SIDE):
+     #               for y in range(0, h, SIDE):
+      #                  x_dif = 0
+       #                 y_dif = 0
+        #                if x + SIDE > w:
+         #                   x_dif = x + SIDE - w
+          #              if y + SIDE > h:
+           #                 y_dif = y + SIDE - h"""
+                pred = eval(photo.image.url[1:])
+                image_list.append(f"{pred}")
+                file.write(rf"{pred}")
             file.close()
             return render(request, 'save.html', {'images': image_list[1:], 'img1': image_list[0],
                                                  'name': str(request.FILES.getlist('photos')[0]) + '.zip'})
